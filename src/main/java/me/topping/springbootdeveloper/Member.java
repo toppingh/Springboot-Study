fd38b83b01a223e9a24d66475f1e8c026bfbcf01
topping <reyna11@naver.com>
2024-08-26 16:08:36 +0900

    0826 ORM 정의 및 실습

--- src/main/java/me/topping/springbootdeveloper/Member.java
+++ src/main/java/me/topping/springbootdeveloper/Member.java
@@ -6,17 +6,22 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본 생성자
 @AllArgsConstructor
 @Getter
-@Entity
+@Entity // Entity로 지정
 public class Member {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id //  id 필드를 기본키로 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // GeneratedValue : 기본키 생성 방식 -> 자동으로 1씩 증가하도록
+    // 기본키를 자동으로 1씩 증가
     @Column(name = "id", updatable = false)
     private Long id; // DB 테이블의 id 컬럼과 매칭
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "name", nullable = false) // name이라는 not null 컬럼과 매핑
     private String name; // DB 테이블의 name 컬럼과 매칭
+
+    public void changeName(String name) {
+        this.name = name;
+    }
 }
